f16753eba5bbacb8751cc2b7970bbe4975bb5803
Krzysztof <k.ukleja@gmail.com>
2019-06-28 22:59:24 +0200

    Lesson 157: calculating ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,15 +5,25 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 const burger = (props) => {
     // Object. is Java Script not React
     // Object.keys - return Array of keys
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             // spread operator ...
             // Array() - it's Java Script
-           return [...Array(props.ingredients[igKey])]
-               .map((_, i) => {
-                  return <BurgerIngredient key={igKey + i} type={igKey}/>;
-               })
-        });
+            return [...Array(props.ingredients[igKey])]
+                .map((_, i) => {
+                    return <BurgerIngredient key={igKey + i} type={igKey}/>;
+                })
+        })
+        // reduce in JS function which allow us to transform Array into something different
+        // to parameters: previous value and current value
+        // [] it's initial value
+        // this flatting array
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
