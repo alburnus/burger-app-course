27550c6aa1cb87aad7b6201f01e8b4e9a5f81cce
Krzysztof <k.ukleja@gmail.com>
2019-06-29 08:39:19 +0200

    Lesson 158: Add controls to burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
 
@@ -18,8 +19,8 @@ class BurgerBuilder extends Component {
     render() {
         return (
             <Auxiliary>
-                <Burger ingredients = {this.state.ingredients}/>
-                <div>Build Controls</div>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls/>
             </Auxiliary>
         );
     }
