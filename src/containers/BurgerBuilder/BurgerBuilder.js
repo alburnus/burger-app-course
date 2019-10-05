d3da860dff5b9e55014af1729b6a2858706e7e4c
Krzysztof <k.ukleja@gmail.com>
2019-10-05 23:40:01 +0200

    Lesson 251: navigating to checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -100,34 +100,35 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         //alert('You continue');
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Krzysztof',
-                address: {
-                    street: 'Street 2',
-                    zipCode: '85485',
-                    country: 'PP'
-                },
-                email: 'a@a.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response =>
-                this.setState({
-                    loading: false,
-                    purchasing: false
-                })
-            )
-            .catch(error =>
-                this.setState({
-                    loading: false,
-                    purchasing: false
-                })
-            );
+        // this.setState({loading: true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Krzysztof',
+        //         address: {
+        //             street: 'Street 2',
+        //             zipCode: '85485',
+        //             country: 'PP'
+        //         },
+        //         email: 'a@a.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response =>
+        //         this.setState({
+        //             loading: false,
+        //             purchasing: false
+        //         })
+        //     )
+        //     .catch(error =>
+        //         this.setState({
+        //             loading: false,
+        //             purchasing: false
+        //         })
+        //     );
+        this.props.history.push('/checkout');
     }
 
     render() {
