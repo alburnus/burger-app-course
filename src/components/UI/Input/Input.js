da1bc16327dbc72dd8cde3d188561b021015f95a
Krzysztof <k.ukleja@gmail.com>
2020-04-05 23:36:10 +0200

    244*: Adding Validation Feedback

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,10 +3,15 @@ import classes from './Input.css';
 
 const input = (props) => {
     let inputElement = null;
+    const inputClasses = [classes.InputElement];
+
+    if (props.invalid && props.shouldValidate) {
+        inputClasses.push(classes.Invalid);
+    }
     switch (props.elementType) {
         case('input'):
             inputElement = <input
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 {...props.elementConfig}
                 value={props.value}
                 onChange={props.changed}
@@ -14,7 +19,7 @@ const input = (props) => {
             break;
         case('textarea'):
             inputElement = <textarea
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 {...props.elementConfig}
                 value={props.value}
                 onChange={props.changed}
@@ -22,9 +27,9 @@ const input = (props) => {
             break;
         case('select'):
             inputElement = <select
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 value={props.value}
-            onChange={props.changed}>
+                onChange={props.changed}>
                 {props.elementConfig.options.map(option => (
                     <option key={option.value} value={option.value}>
                         {option.displayValue}
@@ -34,7 +39,7 @@ const input = (props) => {
             break;
         default:
             inputElement = <input
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 {...props.elementConfig}
                 value={props.value}
                 onChange={props.changed}
