f0f1cafc116e3990a5b39c68478aa226ef7b20c6
Krzysztof <k.ukleja@gmail.com>
2020-05-04 21:58:08 +0200

    348*: Remove console.log from code

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -10,10 +10,6 @@ class Modal extends Component {
             || nextProps.children !== this.props.children;
     }
 
-    componentWillUpdate() {
-        console.log('[Modal] willUpdate');
-    }
-
     render() {
         return (
             <Auxiliary>
