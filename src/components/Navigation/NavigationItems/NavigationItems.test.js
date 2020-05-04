3251a6fa44c6f9cb4ef0654e264983847ab58922
Krzysztof <k.ukleja@gmail.com>
2020-05-04 22:45:06 +0200

    357*: Testing components - continued

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { configure, shallow } from 'enzyme';
+import {configure, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import NavigationItems from './NavigationItems';
@@ -9,8 +9,19 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems/>); // <NavigationItems /> is JSX
+    });
+
     it('should render two navigation item if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems />); // <NavigationItems /> is JSX
         expect(wrapper.find(NavigationItem)).toHaveLength(2); // Now NavigationItem is no JSX
+    });
+
+    it('should render three navigation items if authenticated', () => {
+        // wrapper = shallow(<NavigationItems isAuthenticated/>);
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.find(NavigationItem)).toHaveLength(3); // Now NavigationItem is no JSX
     });
 });
